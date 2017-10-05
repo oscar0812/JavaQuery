787bbee9ade12693e32924c6b9383e2b1efea55e
oscar0812 <oscar0812torres@gmail.com>
2017-10-05 13:57:55 -0500

    some html text for example

--- src/com/httdocs/jquery/MainTest.java
+++ src/com/httdocs/jquery/MainTest.java
@@ -1,14 +1,28 @@
 package com.httdocs.jquery;
 
-
 // ideas:
 // Use hash table to make classes and ids easier to find?
 
-import java.net.URL;
+import com.httdocs.jquery.Html.HtmlFile;
 
 public class MainTest {
-    public static void main(String[] args) throws Exception{
-        HtmlFile file = new HtmlFile("file:///Users/oscartorres/Dropbox/Homepage/main/main_page.html");
-        System.out.println(file.getText());
+    public static void main(String[] args) throws Exception {
+        String html = "<html>\n" +
+                "\n" +
+                "<head>\n" +
+                "  <title></title>\n" +
+                "</head>\n" +
+                "\n" +
+                "<body>\n" +
+                "\n" +
+                "  <h1>some_text</h1>\n" +
+                "  <h3><p>hello</p></h3>\n" +
+                "\n" +
+                "</body>\n" +
+                "\n" +
+                "</html>";
+        HtmlFile file = new HtmlFile(html);
+
+        JQuery jQuery = new JQuery(file);
     }
 }
