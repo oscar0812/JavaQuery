a8818e24ad81e8f143e7f72b032b64a848f91cd5
oscar0812 <oscar0812torres@gmail.com>
2017-10-06 18:18:35 -0500

    added special tags

--- src/com/httdocs/jquery/Html/Constants.java
+++ src/com/httdocs/jquery/Html/Constants.java
@@ -1,6 +1,13 @@
 package com.httdocs.jquery.Html;
 
 public class Constants {
-    public static final char lt = '<';
-    public static final char gt = '>';
+    public static final char LT = '<';
+    public static final char GT = '>';
+
+    public static final String[] VOID_ELEMENTS = new String[]{"area", "base", "basefont", "bgsound", "br", "col",
+            "command", "embed", "frame", "hr", "image", "img", "input", "isindex", "keygen", "link", "menuitem",
+            "meta", "nextid", "param", "source", "track", "wbr"};
+
+    public static final String[] RAW_TEXT_ELEMENTS = new String[]{"script", "style"};
+    public static final String[] ESC_RAW_ELEMENTS = new String[]{"textarea", "title"};
 }
