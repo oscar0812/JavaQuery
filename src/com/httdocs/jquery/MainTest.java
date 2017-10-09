415a6bc1a0dae14904968954a7b9f50efe5c73fb
oscar0812 <oscar0812torres@gmail.com>
2017-10-09 12:55:48 -0500

    Use html from an actual site

--- src/com/httdocs/jquery/MainTest.java
+++ src/com/httdocs/jquery/MainTest.java
@@ -4,25 +4,16 @@ package com.httdocs.jquery;
 // Use hash table to make classes and ids easier to find?
 
 import com.httdocs.jquery.Html.HtmlFile;
+import com.httdocs.jquery.jquery.JQuery;
+import com.httdocs.jquery.jquery.JQueryObject;
 
 public class MainTest {
-    public static void main(String[] args) throws Exception {
-        String html = "<html>\n" +
-                "\n" +
-                "<head>\n" +
-                "  <title></title>\n" +
-                "</head>\n" +
-                "\n" +
-                "<body>\n" +
-                "\n" +
-                "  <h1>some_text</h1>\n" +
-                "  <h3><p>hello</p></h3>\n" +
-                "\n" +
-                "</body>\n" +
-                "\n" +
-                "</html>";
+    public static void main(String[] args) {
+        String html = "http://www.dictionary.com/browse/dog";
         HtmlFile file = new HtmlFile(html);
-
         JQuery jQuery = new JQuery(file);
+        JQueryObject obj = jQuery.getRoot();
+
+        System.out.println(obj.find(".def-set").html());
     }
 }
